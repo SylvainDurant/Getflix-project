04ad691ce8e56d2f0ad4e20f9517ab8cc8d05b5f
cristina <cristinadinca.cd@gmail.com>
2020-12-18 12:14:03 +0100

    finish design profile page

--- layouts/footer.php
+++ layouts/footer.php
@@ -157,6 +157,10 @@
     <!-- Toastr js -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
+    <!-- Bootstrap Datepicker -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw==" crossorigin="anonymous" />
+
     <script src="<?php echo $url[3] == 'pages' ? '..' : '.'; ?>/js/main.js"></script>
 </body>
 </html>
